0d8c9b19aa9ea95b64f1060974fdf052672284d8
naesila <0110124113@student.nurulfikri.ac.id>
2025-03-03 14:43:11 +0700

    upload tugas

---
+++ praktikum-2b/form_belanja.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Form Belanja</title> 
+</head> 
+ 
+<body> 
+    <!-- FORM METHOD POST, ACTION total_belanja.php --> 
+    <!-- input text, name customer --> 
+    <!-- input radio, name produk --> 
+    <!-- input number, name jumlah --> 
+    <!-- input submit, name proses --> 
+</body> 
+
+</html>
---
+++ praktikum-2b/form_nilai.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Nilai</title>
+</head>
+
+<body>
+    <form action="nilai_mahasiswa.php" method="POST">
+        Nama : <input type="text" name="nama" value="" size="6" /></br>
+        Mata Kuliah :
+        <select name="matkul">
+            <optipon value="DDP">Dasar-Dasar Pemrograman</option>
+            <optipon value="BD1">Basis Data</option>
+            <optipon value="Web1">Pemrograman Web</option>
+        </select></br>
+        Nilai UTS : <input type="number" name="nilai_uts" value="" size="6" /></br>
+        Nilai UAS : <input type="number" name="nilai_uas" value="" size="6" /></br>
+        Nilai Tugas/Praktikum : <input type="number" name="nilai_tugas" value="" size="6" /></br>
+
+        <input type="submit" value="Simpan" name="proses" />
+    </form>
+</body>
+
+
+</html>
